2876323ae11abeb9050f8d60be27fdc758d78518
Jiří Kuchyňka <anty150@gmail.com>
2017-06-05 11:28:15 +0200

    added tables: tableRanking, tablePoints; implemented result updating;

--- app/lib/competitionGenerator/allVsAllGenerator.js
+++ app/lib/competitionGenerator/allVsAllGenerator.js
@@ -4,11 +4,15 @@ type ExceptionType = 'Input' | 'Generator' | 'Unsupported';
 
 export class Exception {
   type: ExceptionType; // Tells what causet the exception
-  message: string // Some kind of text, that tells more about problem. Now only in English.
+  message: string // Some kind of text, that tells more about problem.
+  messageEn: string // Message in English.
+  messageCz: string // Message in Czech.
 
-  constructor(type: ExceptionType, message: string) {
+  constructor(type: ExceptionType, messageEn: string, messageCz: string) {
     this.type = type;
-    this.message = message;
+    this.message = messageCz;
+    this.messageEn = messageEn;
+    this.messageCz = messageCz;
   }
 
   toString() {
@@ -138,6 +142,10 @@ function makeCalcTmp(input: Input): CalcTmp {
   };
 }
 
+export type TeamPoints = {
+  total: number
+};
+
 export type Result = {
   input: Input, // Source of informations for generating result
   resultTmp: ResultTmp, // Holds state of generating.
@@ -145,19 +153,25 @@ export type Result = {
   allMatches: { [indexId: number]: Match }, // Map of all matches,
   // everywhere else is only indexes ("names") of matches.
   tablesSeasons: Array<SeasonTable>, // Array of Season tables.
-  tablesAllVsAll: Array<AllVsAllTable> // Array of AllVsAll tables
+  tablesAllVsAll: Array<AllVsAllTable>, // Array of AllVsAll tables
+  tableRanking: Array<number>,
+  tablePoints: Array<TeamPoints>
 };
 
-export function generate(input: Input): Result {
-  const result: Result = {
+function makeResult(input: Input): Result {
+  return {
     input,
     resultTmp: makeResultTmp(input),
     allMatches: {},
     tablesSeasons: [],
     tablesAllVsAll: [],
+    tableRanking: Array(input.teams.length).fill(-1).map((_, i) => i),
+    tablePoints: Array(input.teams.length).fill(null).map(() => ({ total: 0 })),
   };
+}
 
-  return update(result);
+export function generate(input: Input): Result {
+  return update(makeResult(input));
 }
 
 function addToAllVsAllTable(resultIn: Result, matchIndex: number) {
@@ -204,7 +218,8 @@ function whoShouldPlay(input: Input, resultTmp: ResultTmp, calcTmp: CalcTmp,
           if (input.maxMatchesOfOneTeamInSeason !== -1 &&
               resultTmp.teamsStatus[teamIndex].seasonsMatches
                 >= input.maxMatchesOfOneTeamInSeason) {
-            throw new Exception('Generator', 'Team reached maximum matches in round and maximal pauses consecutively.');
+            throw new Exception('Generator', 'Team reached maximum matches in season and maximal pauses in a row consecutively.',
+                                'Došlo k dosažení maxima zápasů v herním období a maximum přestávek po sobě u jednoho týmu současně.');
           }
           return teamIndex;
         }
@@ -247,131 +262,180 @@ function whoShouldPlay(input: Input, resultTmp: ResultTmp, calcTmp: CalcTmp,
 export function update(resultIn: Result): Result {
   const result = resultIn;
 
-  if (result.resultTmp.nextMatchesIndexPointer !== 0) throw new Exception('Unsupported', 'Updating is not currently supported.');
-  if (result.input.teams.length < 2) throw new Exception('Input', 'Two teams at last required for match generating.');
+  // if (result.resultTmp.nextMatchesIndexPointer !== 0) {
+  //   throw new Exception('Unsupported', 'Updating is not currently supported.',
+  //                       'Aktualizování soutěže zatím není podporováno.');
+  // }
 
-  const calcTmp = makeCalcTmp(result.input);
+  if (result.resultTmp.nextMatchesIndexPointer === 0) {
+    if (result.input.teams.length < 2) {
+      throw new Exception('Input', 'Two teams at last required for competition generating.',
+                          'Minimálně 2 týmy jsou vyžadovány pro generování soutěže.');
+    }
 
-  for (let seasonIndex = 0; seasonIndex < result.input.seasonsCount; seasonIndex++) {
-    const season: SeasonTable = Array(result.input.maxMatchesInSeason).fill(null);
-    for (let matchInSeasonIndex = 0; matchInSeasonIndex < season.length; matchInSeasonIndex++) {
-      const teamIndex1: number = whoShouldPlay(result.input, result.resultTmp,
-                                               calcTmp, seasonIndex, -1);
-      let teamIndex2: number = -1;
-      if (teamIndex1 !== -1) {
-        teamIndex2 = whoShouldPlay(result.input, result.resultTmp,
-                                   calcTmp, seasonIndex, teamIndex1);
+    const calcTmp = makeCalcTmp(result.input);
+
+    for (let seasonIndex = 0; seasonIndex < result.input.seasonsCount; seasonIndex++) {
+      const season: SeasonTable = Array(result.input.maxMatchesInSeason).fill(null);
+      for (let matchInSeasonIndex = 0; matchInSeasonIndex < season.length; matchInSeasonIndex++) {
+        const teamIndex1: number = whoShouldPlay(result.input, result.resultTmp,
+                                                 calcTmp, seasonIndex, -1);
+        let teamIndex2: number = -1;
+        if (teamIndex1 !== -1) {
+          teamIndex2 = whoShouldPlay(result.input, result.resultTmp,
+                                     calcTmp, seasonIndex, teamIndex1);
+        }
+
+        const team1: Team | null = teamIndex1 === -1 ? null : result.input.teams[teamIndex1];
+        const team2: Team | null = teamIndex2 === -1 ? null : result.input.teams[teamIndex2];
+
+        if (team1 === null || team2 === null) {
+          calcTmp.teamsIndexesLast.forEach((teamIndex: number) => {
+            result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
+          });
+          calcTmp.teamsIndexesLast = [];
+
+          season[matchInSeasonIndex] = null;
+        } else {
+          const oldLastTeams: Array<number> = calcTmp.teamsIndexesLast;
+          const newLastTeams: Array<number> = [teamIndex1, teamIndex2];
+          oldLastTeams.forEach((teamIndex: number) => {
+            if (!newLastTeams.includes(teamIndex)) {
+              result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
+            }
+          });
+          newLastTeams.forEach((teamIndex: number) => {
+            if (!oldLastTeams.includes(teamIndex)) {
+              result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
+            }
+          });
+          calcTmp.teamsIndexesLast = newLastTeams;
+
+          result.resultTmp.teamsStatus[teamIndex1].seasonsMatches++;
+          result.resultTmp.teamsStatus[teamIndex2].seasonsMatches++;
+          result.resultTmp.teamsStatus[teamIndex1].actualMatches++;
+          result.resultTmp.teamsStatus[teamIndex2].actualMatches++;
+
+          result.resultTmp.pairs[teamIndex1][teamIndex2]++;
+          result.resultTmp.pairs[teamIndex2][teamIndex1]++;
+
+          const nextMatchIndex: number = result.resultTmp.nextMatchesIndexPointer;
+          result.resultTmp.nextMatchesIndexPointer++;
+          result.allMatches[nextMatchIndex] = {
+            team1Index: teamIndex1,
+            team2Index: teamIndex2,
+          };
+          season[matchInSeasonIndex] = nextMatchIndex;
+          addToAllVsAllTable(result, nextMatchIndex);
+        }
+
+        for (let teamIndex = 0; teamIndex < result.input.teams.length; teamIndex++) {
+          result.resultTmp.teamsStatus[teamIndex].consecutive++;
+          if (result.input.maxOneTeamMatchesConsecutivelyInSeason !== -1 &&
+              calcTmp.teamsIndexesLast.includes(teamIndex) &&
+              result.resultTmp.teamsStatus[teamIndex].consecutive
+                > result.input.maxOneTeamMatchesConsecutivelyInSeason) {
+            throw new Exception('Generator', 'Too much teams can\'t play at same time.',
+                                'Příliš mnoho týmů nemůže hrát ve stejnou dobu.');
+          }
+          if (result.input.maxOneTeamPausesConsecutivelyInSeason !== -1 &&
+              !result.input.teams[teamIndex].forbiddenSeasonsIndexes.includes(seasonIndex) &&
+              !calcTmp.teamsIndexesLast.includes(teamIndex) &&
+              result.resultTmp.teamsStatus[teamIndex].consecutive
+                > result.input.maxOneTeamPausesConsecutivelyInSeason) {
+            throw new Exception('Generator', 'More then two teams must play at same time.',
+                                'Více jak dva týmy musí hrát ve stejnou chvíly.');
+          }
+        }
+
+        calcTmp.lastMatchesEquation++;
+        const targetActualMatches: number = result.resultTmp
+          .teamsStatus[calcTmp.teamsIndexes[0]].actualMatches;
+        const targetPairsMatches: number = result.resultTmp
+          .pairs[calcTmp.teamsIndexes[0]][calcTmp.teamsIndexes[1]];
+        const equals = calcTmp.teamsIndexes.every((teamIndex: number) =>
+          result.resultTmp.teamsStatus[teamIndex].actualMatches === targetActualMatches &&
+              calcTmp.teamsIndexes.every((rivalTeamIndex: number) =>
+                teamIndex === rivalTeamIndex ||
+                result.resultTmp.pairs[teamIndex][rivalTeamIndex] === targetPairsMatches,
+              ),
+        );
+        if (equals) calcTmp.lastMatchesEquation = 0;
+        /*
+        console.log(`"equation": ${lastEquation}
+           \n"round": ${(i + 1)}
+           \n"match": ${(j + 1)}
+           \n"matches": ${(i * matches.length) + (j + 1)}
+           \n"tmp": ${tmp.toString()}`);
+        */
       }
 
-      const team1: Team | null = teamIndex1 === -1 ? null : result.input.teams[teamIndex1];
-      const team2: Team | null = teamIndex2 === -1 ? null : result.input.teams[teamIndex2];
+      calcTmp.teamsIndexes.forEach((teamIndex: number) => {
+        result.resultTmp.teamsStatus[teamIndex].seasonsMatches = 0;
+        result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
+      });
+      result.tablesSeasons[seasonIndex] = season;
+    }
 
-      if (team1 === null || team2 === null) {
-        calcTmp.teamsIndexesLast.forEach((teamIndex: number) => {
-          result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
-        });
-        calcTmp.teamsIndexesLast = [];
+    if (!result.input.allowIncompleteTable && calcTmp.lastMatchesEquation !== 0) {
+      if (calcTmp.lastMatchesEquation ===
+          (result.input.seasonsCount * result.input.maxMatchesInSeason)) {
+        throw new Exception('Generator', 'Can\'t create equality of played matches for all teams.',
+                            'Nelze vytvořit rovnost zápasů všech týmů, příliš málo zápasů.');
+      }
 
-        season[matchInSeasonIndex] = null;
+      console.log(`Reverting last ${calcTmp.lastMatchesEquation} matches to get equality of played matches for all teams.`);
+      for (let i = result.input.seasonsCount - 1; i >= 0; i--) {
+        for (let j = result.input.maxMatchesInSeason - 1; j >= 0; j--) {
+          const matchIndex: number | null = result.tablesSeasons[i][j];
+          if (matchIndex !== null) {
+            result.tablesSeasons[i][j] = null;
+            delete result.allMatches[matchIndex];
+          }
+          if (--calcTmp.lastMatchesEquation === 0) break;
+        }
+        if (calcTmp.lastMatchesEquation === 0) break;
+      }
+      result.tablesAllVsAll.pop();
+    }
+  }
+
+  result.tablePoints.map(() => ({ total: 0 }));
+  Object.keys(result.allMatches) // eslint-disable-line flowtype-errors/show-errors
+    .forEach((indexId: number) => {
+      const match: Match = result.allMatches[indexId];
+      if (!match.result) return;
+      const team1Points: TeamPoints = result.tablePoints[match.team1Index];
+      const team2Points: TeamPoints = result.tablePoints[match.team2Index];
+      if (match.result.team1Score && match.result.team2Score) {
+        const team1Score: number =
+          match.result.team1Score; // eslint-disable-line flowtype-errors/show-errors
+        const team2Score: number =
+          match.result.team2Score; // eslint-disable-line flowtype-errors/show-errors
+        if (team1Score > team2Score) {
+          team1Points.total += 3;
+        } else if (team2Score > team1Score) {
+          team2Points.total += 3;
+        } else {
+          team1Points.total += 1;
+          team2Points.total += 1;
+        }
       } else {
-        const oldLastTeams: Array<number> = calcTmp.teamsIndexesLast;
-        const newLastTeams: Array<number> = [teamIndex1, teamIndex2];
-        oldLastTeams.forEach((teamIndex: number) => {
-          if (!newLastTeams.includes(teamIndex)) {
-            result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
-          }
-        });
-        newLastTeams.forEach((teamIndex: number) => {
-          if (!oldLastTeams.includes(teamIndex)) {
-            result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
-          }
-        });
-        calcTmp.teamsIndexesLast = newLastTeams;
-
-        result.resultTmp.teamsStatus[teamIndex1].seasonsMatches++;
-        result.resultTmp.teamsStatus[teamIndex2].seasonsMatches++;
-        result.resultTmp.teamsStatus[teamIndex1].actualMatches++;
-        result.resultTmp.teamsStatus[teamIndex2].actualMatches++;
-
-        result.resultTmp.pairs[teamIndex1][teamIndex2]++;
-        result.resultTmp.pairs[teamIndex2][teamIndex1]++;
-
-        const nextMatchIndex: number = result.resultTmp.nextMatchesIndexPointer;
-        result.resultTmp.nextMatchesIndexPointer++;
-        result.allMatches[nextMatchIndex] = {
-          team1Index: teamIndex1,
-          team2Index: teamIndex2,
-        };
-        season[matchInSeasonIndex] = nextMatchIndex;
-        addToAllVsAllTable(result, nextMatchIndex);
-      }
-
-      for (let teamIndex = 0; teamIndex < result.input.teams.length; teamIndex++) {
-        result.resultTmp.teamsStatus[teamIndex].consecutive++;
-        if (result.input.maxOneTeamMatchesConsecutivelyInSeason !== -1 &&
-            calcTmp.teamsIndexesLast.includes(teamIndex) &&
-            result.resultTmp.teamsStatus[teamIndex].consecutive
-              > result.input.maxOneTeamMatchesConsecutivelyInSeason) {
-          throw new Exception('Generator', 'Too much teams can\'t play at same time.');
-        }
-        if (result.input.maxOneTeamPausesConsecutivelyInSeason !== -1 &&
-            !result.input.teams[teamIndex].forbiddenSeasonsIndexes.includes(seasonIndex) &&
-            !calcTmp.teamsIndexesLast.includes(teamIndex) &&
-            result.resultTmp.teamsStatus[teamIndex].consecutive
-              > result.input.maxOneTeamPausesConsecutivelyInSeason) {
-          throw new Exception('Generator', 'More then two teams must play at same time.');
+        const winnerTeamIndex: number = match.result.winnerTeamIndex;
+        if (winnerTeamIndex === match.team1Index) {
+          team1Points.total += 3;
+        } else if (winnerTeamIndex === match.team2Index) {
+          team2Points.total += 3;
+        } else {
+          team1Points.total += 1;
+          team2Points.total += 1;
         }
       }
-
-      calcTmp.lastMatchesEquation++;
-      const targetActualMatches: number = result.resultTmp
-        .teamsStatus[calcTmp.teamsIndexes[0]].actualMatches;
-      const targetPairsMatches: number = result.resultTmp
-        .pairs[calcTmp.teamsIndexes[0]][calcTmp.teamsIndexes[1]];
-      const equals = calcTmp.teamsIndexes.every((teamIndex: number) =>
-        result.resultTmp.teamsStatus[teamIndex].actualMatches === targetActualMatches &&
-            calcTmp.teamsIndexes.every((rivalTeamIndex: number) =>
-              teamIndex === rivalTeamIndex ||
-              result.resultTmp.pairs[teamIndex][rivalTeamIndex] === targetPairsMatches,
-            ),
-      );
-      if (equals) calcTmp.lastMatchesEquation = 0;
-      /*
-      console.log(`"equation": ${lastEquation}
-         \n"round": ${(i + 1)}
-         \n"match": ${(j + 1)}
-         \n"matches": ${(i * matches.length) + (j + 1)}
-         \n"tmp": ${tmp.toString()}`);
-      */
-    }
-
-    calcTmp.teamsIndexes.forEach((teamIndex: number) => {
-      result.resultTmp.teamsStatus[teamIndex].seasonsMatches = 0;
-      result.resultTmp.teamsStatus[teamIndex].consecutive = 0;
     });
-    result.tablesSeasons[seasonIndex] = season;
-  }
 
-  if (!result.input.allowIncompleteTable && calcTmp.lastMatchesEquation !== 0) {
-    if (calcTmp.lastMatchesEquation ===
-        (result.input.seasonsCount * result.input.maxMatchesInSeason)) {
-      throw new Exception('Generator', 'Can\'t create equality of played matches for all teams.');
-    }
-
-    console.log(`Reverting last ${calcTmp.lastMatchesEquation} matches to get equality of played matches for all teams.`);
-    for (let i = result.input.seasonsCount - 1; i >= 0; i--) {
-      for (let j = result.input.maxMatchesInSeason - 1; j >= 0; j--) {
-        const matchIndex: number | null = result.tablesSeasons[i][j];
-        if (matchIndex !== null) {
-          result.tablesSeasons[i][j] = null;
-          delete result.allMatches[matchIndex];
-        }
-        if (--calcTmp.lastMatchesEquation === 0) break;
-      }
-      if (calcTmp.lastMatchesEquation === 0) break;
-    }
-    result.tablesAllVsAll.pop();
-  }
+  result.tableRanking.sort((teamIndex1: number, teamIndex2: number) =>
+    result.tablePoints[teamIndex2].total - result.tablePoints[teamIndex1].total,
+  );
 
   return result;
 }
